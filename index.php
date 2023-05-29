439552b0ee519d6c3cc0a934107fd619cb553891
ADEP-123 <andep210399@gmail.com>
2023-05-29 16:55:46 -0500

    feat: :sparkles: 6. Funciones de salida en PHP

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-    //Aqui va todo el codigo en PHP
+    /**
+     * *Funciones de salida
+     * *todo lo que se imprime va directamente a la pagina
+     */
+
+     /**
+      * *echo fues la mas comun
+      */
+     echo 'Texto a imprimir </br>';
+
+     /**
+      ** print solo puede imprimir una cadena de texto a la vez 
+      ** El marcador %s lo que hace es que ahi se va a colocar lo que siga en la siguiente coma dependiendo cuantos de estos marcadores haya
+      */
+    $texto = 'Mundo';
+    printf('Hola %s', $texto);
+
+    /* sprintf es similar a printf pero no imprime sino que asigna a una variable, es decir retorna */
+
+    $texto = 'Mundo';
+    $mensaje = sprintf('Hola %s', $texto);
+    echo $mensaje;
+
+    
 ?>
