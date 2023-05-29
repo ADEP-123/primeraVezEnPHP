817a232f8d9de577222083cedc2e36b32ff3c9ff
ADEP-123 <andep210399@gmail.com>
2023-05-29 17:06:38 -0500

    feat: :sparkles: 8 Tipos de datos

--- index.php
+++ index.php
@@ -1,32 +1,25 @@
 <?php
 /**
- **Variables y constantes
- **se usa el simbolo $ seguido del nombre
- ** la declaracion estricta asigna el tipo de variable en parentesis despues de el igual y despues de estos el valor de la variable 
+ **Tipos de datos
  */
 
-/* numerica */
-$edad = 25;
+ /* Boolean */
+ $logueado = true;
+ var_dump($logueado);
 
-/* Declarar una variable de texto */
-$nombre = 'Juan';
+ /* Enetros */
+ $numero = 200;
+ var_dump($numero);
 
-/* Declarar una variable booleana */
-$es_valido = true;
-echo var_dump($nombre);
-/* vardump devuelve el tipo de variable y su magnitud */
+ /* Floats */
+ $float = 200.5;
+ var_dump($float);
 
-/**
- ** Declarar una constante numercia 
- ** Para declarar una constante se utiliza el define, entre parentesis va el nombre de la variable seguido por coma y el valor
- */
+ /* Strings */
+ $nombre = 'Juan';
+ var_dump($nombre);
 
- /* numerica */
- define('PI', 3.14159264);
-
- /* texto */
- define('SALUDO', 'Hola mundo');
-
- /* Declarar una constante booleana */
- define('ES_VALIDO', true);
+ /* Strings */
+ $array = [];
+ var_dump($array);
 ?>
