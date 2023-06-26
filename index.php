c7b1f0ba76be630f99b0aeb97df280ac729a7865
ADEP-123 <andep210399@gmail.com>
2023-06-26 18:24:07 -0500

    feat: :sparkles: Json Encode

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <?php
 
 /**
- * ? include, require, include_once, require_once
+ * ? Json_encode y json_decode
  * * Son funciones que se utilizan para incluir archivos externos en un programa.
- * ? 1. include e include_once
- * * Permiten incluir un archivo PHP en el programa. La diferencia entre ellas es que include puede incluir el mismo archivo varias veces mientras que include_once asegura que el archivo solo se incluya una vez en el programa.
+ * ? 1. json_encode()
+ * * Se utiliza para convertir una estructura de datos en PHP en una cadena JSON.
  */
-// Incluye el archivo "funciones.php" una sola vez
-include_once 'funciones.php';
+// Array
+$datos = array(
+    'nombre' => 'Juan',
+    'edad' => 30,
+    'hobbies' => array('fútbol', 'tenis', 'guitarra'),
+    'direccion' => array(
+        'calle' => 'Avenida Principal',
+        'numero' => 123,
+        'ciudad' => 'Ciudad de Ejemplo'
+    )
+);
 
-// Llama a la función saludar()
-saludar();
+// Convierte el array en formato JSON
+$json = json_encode($datos);
 
-// Accede a la variable $nombre definida en "funciones.php"
-echo "El nombre es: " . $nombre;
-
-// Crea una instancia de la clase MiClase definida en "funciones.php"
-$objeto = new MiClase();
-/**
- * ? 2. require require_once
- * * En caso de que el archivo no pueda ser encontrado, se detendrá la ejecución del programa. Al utilizar require_once, se asegura que el archivo solo se incluya una vez en el programa.
- */
-require_once 'configuracion.php';
-//El archivo configuracion.php no existe por lo cual se detiene la ejecucion del programa
-echo "Esto no deberia aparecer";
+// Imprime el resultado
+echo $json;
