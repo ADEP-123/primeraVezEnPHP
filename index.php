4c4fcf8bb78294ca5db2c6374ce685c73abc0059
ADEP-123 <andep210399@gmail.com>
2023-06-26 18:21:29 -0500

    feat: :sparkles: requie, require_once

--- index.php
+++ index.php
@@ -17,3 +17,10 @@ echo "El nombre es: " . $nombre;
 
 // Crea una instancia de la clase MiClase definida en "funciones.php"
 $objeto = new MiClase();
+/**
+ * ? 2. require require_once
+ * * En caso de que el archivo no pueda ser encontrado, se detendrá la ejecución del programa. Al utilizar require_once, se asegura que el archivo solo se incluya una vez en el programa.
+ */
+require_once 'configuracion.php';
+//El archivo configuracion.php no existe por lo cual se detiene la ejecucion del programa
+echo "Esto no deberia aparecer";
