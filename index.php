cb721190ebbcaed0258c8262b48c0af7d614a9f4
ADEP-123 <andep210399@gmail.com>
2023-06-26 18:28:18 -0500

    feat: :sparkles: json_decode true

--- index.php
+++ index.php
@@ -28,5 +28,12 @@ echo $json;
  * ? 2. json_decode()
  * * Se utiliza para convertir una cadena JSON en una estructura de datos de PHP.
  */
-$json = json_decode($json);
+// $json = json_decode($json);
+// var_dump($json);
+
+/**
+ * ? 2.1 json_decode(,true)
+ * * En caso de que se desee manipular el objeto de tipo PHP como un arreglo asociativo se puede agregar un segundo argumento en la función json-decode La cual es un valor booleano; en caso de que se le pase un valor true la función retornará un arreglo de tipo asociativo
+ */
+$json = json_decode($json, true);
 var_dump($json);
