f0e6ec3eb5f0b5063f20ea1c91409e057dcb156c
ADEP-123 <andep210399@gmail.com>
2023-06-26 17:35:57 -0500

    feat: :sparkles: Estructuras de control, condicionales, switch

--- index.php
+++ index.php
@@ -23,3 +23,27 @@ if ($numero % 2 == 0) {
 } else {
     echo "El número es impar.";
 }
+/**
+ * ? 1.2. Switch ermite ejecutar diferentes bloques de código dependiendo del valor de una variable.
+ */
+$opcion = 2;
+
+// Selecciona el caso dependiendo del valor de $opcion
+switch ($opcion) {
+    case 1:
+        // Si $opcion es 1, se ejecutará este bloque de código
+        echo "Opción 1 seleccionada.";
+        break;
+    case 2:
+        // Si $opcion es 2, se ejecutará este bloque de código
+        echo "Opción 2 seleccionada.";
+        break;
+    case 3:
+        // Si $opcion es 3, se ejecutará este bloque de código
+        echo "Opción 3 seleccionada.";
+        break;
+    default:
+        // Si $opcion no coincide con ninguno de los casos anteriores, se ejecutará este bloque de código
+        echo "Opción no válida.";
+        break;
+}
