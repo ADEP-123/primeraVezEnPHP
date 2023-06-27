96ea98938fdf3b0306cfc116228c3dc18d41418b
ADEP-123 <andep210399@gmail.com>
2023-06-26 20:14:33 -0500

    feat: :sparkles: Fundamentos Mysql

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
 
 /**
- * ? COMPOSER
- * * Administrador de dependencias para PHP que permite instalar librerías de terceros para facilitar el desarrollo con php
- * * Una vez que tienes Composer instalado, mediante el uso de la terminal puede configurar composer con el comando 'composer init'
- *
- * * Además de gestionar las dependencias, Composer también proporciona una forma fácil de cargar automáticamente las clases utilizando su autoloading. Cuando ejecutas 'composer install', Composer generará un archivo 'autoload.php' en la carpeta 'vendor' que puedes incluir en tu archivo principal para cargar automáticamente las clases del proyecto.
- *
- * * Para utilizar el autoload de Composer, simplemente incluye el archivo 'autoload.php' en tu archivo principal antes de utilizar las clases. El autoload de Composer se encargará de cargar automáticamente las clases según los estándares PSR-4 con la siguiente linea de codigo;
- * "require": {},
- * "autoload":{
- *  "psr-4":{
- *      "App\\":"./clases"
- *  }
- * }
- * * Posteriormente debe ejecutar el comando composer update
- * * Finalmente en el app debe requerir el autoload el cual se encuentra dentro de la carpeta vendor con el comando "require 'vendor/autoload.php;'
+ * ? PHP Y MSQL
+ * ? Bases de datos relacionales
+ * * Son un tipo de sistema de gestión de bases de datos (SGBD) que organiza la información en tablas estructuradas y establece relaciones entre ellas
+ * * Las bases de datos relacionales se basan en el lenguaje de consulta estructurado (SQL, por sus siglas en inglés) para interactuar con los datos. SQL proporciona un conjunto de comandos y sentencias para realizar consultas, inserciones, actualizaciones y eliminaciones de datos en la base de datos
+ * ? Entidades
+ * * Categorías de cosas que son importantes para un negocio y sobre las que se debe conservar información
+ * ? Atributos
+ * * Las entidades contienen datos e información que el negocio debe conocer y recordar
+ * ? Identificadores unicos
+ * * Un identificador único es un atributo o conjunto de atributos que permite identificar de manera exclusiva cada registro en una tabla. Su función principal es garantizar la integridad y la unicidad de los datos almacenados.
+ * ? Relaciones
+ * * Representa las reglas de negocio que enlazan entidades. Cada relación siempre tiene dos reglas de negocio, por ejemplo un departamento puesde tener uno o varios municipios mientras que a cada municipio se le debe asignar un unico departamento
+ * ? Clave ajena o foranea
+ * * Son atributos o conjuntos de atributos que se utilizan para establecer relaciones entre dos tablas en una base de datos relacional. Estas claves foráneas permiten mantener la integridad referencial y establecer la conexión entre los registros de diferentes tablas.
+ *  ? Normalizacion de la base de datos
+ * * Proceso de diseño que se utiliza para organizar y estructurar las tablas de una base de datos relacional de manera eficiente y libre de redundancias. El objetivo principal de la normalización es eliminar la duplicación de datos y garantizar la integridad y consistencia de la información almacenada
  */
